86f3c2e5dd2e28356f08b192960dfdc2ec7fe382
Kevin Meredith <Kevin.m.meredith@gmail.com>
2015-01-05 00:18:15 -0500

    added flatten attempt - it's not total though.

--- sandbox/Flatten.hs
+++ sandbox/Flatten.hs
@@ -1,7 +1,11 @@
--- flatten List, i.e. remove Nothing's, keep the Just's
-flatten :: [Maybe a] -> [Maybe a]
-flatten xs = xs >>= f
+-- flatten List, i.e. remove Nothing's, keep the Just's & extract the `a`'s.
+flatten :: [Maybe a] -> [a]
+flatten xs = map g $ xs >>= f
 
 f :: Maybe a -> [Maybe a]
 f x = case x of Just _  -> [x]
                 Nothing -> []
+
+-- partial function!
+g :: Maybe a -> a
+g (Just x) = x
